123ace7371bca29f3c4ba578bb0772bb7f3572e4
dudenies <160150201+dudenies@users.noreply.github.com>
2024-02-23 16:25:48 +0530

    Update api.routes.js

--- src/router/api.routes.js
+++ src/router/api.routes.js
@@ -1,7 +1,7 @@
 // const BASE_API_URL = "https://aptasksynthflow-apiendpoint.vercel.app/";
 
 // const BASE_API_URL ="http://127.0.0.1:5000/"
-const BASE_API_URL = "https://gptvoiceapi.vercel.app/"
+const BASE_API_URL = "https://apiendpointss.vercel.app/"
 
 // Scaled Order API URLs
 export const CAMPAIGN_TEST_API_URL = BASE_API_URL + "campaignTest";
